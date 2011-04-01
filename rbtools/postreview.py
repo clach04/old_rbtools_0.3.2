78fe517209589fa1e7e0591fd41f7c75a8f26819
unknown <clach04@.ingres.prv>
2011-04-01 11:48:21 -0700

    Fixed previous (bad) merge was bad. Made pkg_resources (setuptools from http://peak.telecommunity.com/) dependency optional

--- rbtools/postreview.py
+++ rbtools/postreview.py
@@ -17,7 +17,72 @@ import datetime
 import urllib
 import urllib2
 from optparse import OptionParser
-from pkg_resources import parse_version
+try:
+    # setuptools from http://peak.telecommunity.com/
+    from pkg_resources import parse_version
+except ImportError:
+    """Either
+        steal the entire pkg_resources.py as it doesn't seem to have any external dependencies
+    or
+        steal parse_version() from pkg_resources.py 
+    """
+    component_re = re.compile(r'(\d+ | [a-z]+ | \.| -)', re.VERBOSE)
+    replace = {'pre':'c', 'preview':'c','-':'final-','rc':'c','dev':'@'}.get
+
+    def _parse_version_parts(s):
+        for part in component_re.split(s):
+            part = replace(part,part)
+            if not part or part=='.':
+                continue
+            if part[:1] in '0123456789':
+                yield part.zfill(8)    # pad for numeric comparison
+            else:
+                yield '*'+part
+
+        yield '*final'  # ensure that alpha/beta/candidate are before final
+
+    def parse_version(s):
+        """Convert a version string to a chronologically-sortable key
+
+        This is a rough cross between distutils' StrictVersion and LooseVersion;
+        if you give it versions that would work with StrictVersion, then it behaves
+        the same; otherwise it acts like a slightly-smarter LooseVersion. It is
+        *possible* to create pathological version coding schemes that will fool
+        this parser, but they should be very rare in practice.
+
+        The returned value will be a tuple of strings.  Numeric portions of the
+        version are padded to 8 digits so they will compare numerically, but
+        without relying on how numbers compare relative to strings.  Dots are
+        dropped, but dashes are retained.  Trailing zeros between alpha segments
+        or dashes are suppressed, so that e.g. "2.4.0" is considered the same as
+        "2.4". Alphanumeric parts are lower-cased.
+
+        The algorithm assumes that strings like "-" and any alpha string that
+        alphabetically follows "final"  represents a "patch level".  So, "2.4-1"
+        is assumed to be a branch or patch of "2.4", and therefore "2.4.1" is
+        considered newer than "2.4-1", which in turn is newer than "2.4".
+
+        Strings like "a", "b", "c", "alpha", "beta", "candidate" and so on (that
+        come before "final" alphabetically) are assumed to be pre-release versions,
+        so that the version "2.4" is considered newer than "2.4a1".
+
+        Finally, to handle miscellaneous cases, the strings "pre", "preview", and
+        "rc" are treated as if they were "c", i.e. as though they were release
+        candidates, and therefore are not as new as a version string that does not
+        contain them, and "dev" is replaced with an '@' so that it sorts lower than
+        than any other pre-release tag.
+        """
+        parts = []
+        for part in _parse_version_parts(s.lower()):
+            if part.startswith('*'):
+                if part<'*final':   # remove '-' before a prerelease tag
+                    while parts and parts[-1]=='*final-': parts.pop()
+                # remove trailing zeros from each series of numeric parts
+                while parts and parts[-1]=='00000000':
+                    parts.pop()
+            parts.append(part)
+        return tuple(parts)
+
 from tempfile import mkstemp
 from urlparse import urljoin, urlparse
 
@@ -4454,7 +4519,16 @@ def main():
 
     if (isinstance(tool, PerforceClient) or
         isinstance(tool, PlasticClient)) and changenum is not None:
-    
+        changenum = tool.sanitize_changenum(changenum)
+
+        # NOTE: In Review Board 1.5.2 through 1.5.3.1, the changenum support
+        # is broken, so we have to force the deprecated API.
+        if (parse_version(server.rb_version) >= parse_version('1.5.2') and
+            parse_version(server.rb_version) <= parse_version('1.5.3.1')):
+            debug('Using changenums on Review Board %s, which is broken. '
+                  'Falling back to the deprecated 1.0 API' % server.rb_version)
+            server.deprecated_api = True
+
     ################################################################
     if diff and isinstance(tool, PiccoloClient) and options.p2_guess_branch and options.branch is None:
         options.branch = tool.guess_branch(diff)
@@ -4542,16 +4616,6 @@ Design and documentation Links:
             options.description = options.description.replace('    Candidate for merging into EDITME_CODELINE(S) after submission into this codeline.', '    Candidate for merging into EDITME_CODELINE(S) after submission into this codeline.\n    Not a Candidate for merging into other codeline(s).')
     ################################################################
     
-    if isinstance(tool, PerforceClient) and changenum is not None:
-        changenum = tool.sanitize_changenum(changenum)
-
-        # NOTE: In Review Board 1.5.2 through 1.5.3.1, the changenum support
-        #       is broken, so we have to force the deprecated API.
-        if (parse_version(server.rb_version) >= parse_version('1.5.2') and
-            parse_version(server.rb_version) <= parse_version('1.5.3.1')):
-            debug('Using changenums on Review Board %s, which is broken. '
-                  'Falling back to the deprecated 1.0 API' % server.rb_version)
-            server.deprecated_api = True
 
     if options.output_diff_only:
         # The comma here isn't a typo, but rather suppresses the extra newline
