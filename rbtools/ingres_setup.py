07caf5814a05778266d0b65f4277b26919a082f0
unknown <clach04@clach04-790.ingres.prv>
2012-03-06 11:26:40 -0800

    Added sample build comment to Ingres setup.py

--- rbtools/ingres_setup.py
+++ rbtools/ingres_setup.py
@@ -4,6 +4,7 @@ Issue:
 NOTE using Python 2.4, this results in an exe about 4Mb in size.
 NOTE using Python 2.6, this results in an exe about 5.5Mb in size.
 
+    E:\Python24\python.exe ingres_setup.py py2exe
     c:\python24\python ingres_setup.py py2exe
     setup.py py2exe
 
